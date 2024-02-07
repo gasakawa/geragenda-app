3d0faf339f0457ad4c6ba5c16be1c431ee3d37ae
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2024-02-07 15:27:18 -0300

    feat: add a menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,22 @@
 import Link from 'next/link';
 import Logo from '../../public/geragenda-logo-white.png';
+import LogoWhite from '../../public/logo-white.svg';
 import Image from 'next/image';
 import { Button } from './ui/button';
+import Menu from './Menu';
 
 export async function Header() {
   return (
     <header className="bg-blue-800 flex items-center fixed w-[100vw] min-h-[10vh] py-5">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <Image src={Logo} alt="GerAgenda" />
+          <Image src={Logo} priority alt="GerAgenda" className="hidden lg:flex" />
+          <Image src={LogoWhite} alt="GerAgenda" className="flex lg:hidden" />
         </Link>
 
-        <div className="flex items-center gap-x-5">
+        <Menu />
+
+        <div className="items-center gap-x-5 hidden lg:flex">
           <div className="flex items-center gap-x-5">
             <Button
               className="bg-transparent border-slate-50 border text-lg font-bold hover:bg-white hover:text-blue-800"
---
+++ src/components/Menu.tsx
@@ -0,0 +1,13 @@
+function Menu() {
+  return (
+    <div className="hidden lg:flex">
+      <ul className="flex flex-nowrap">
+        <li className="text-white text-md py-0 px-[13px] font-medium">Recursos</li>
+        <li className="text-white text-md py-0 px-[13px] font-medium">Planos</li>
+        <li className="text-white text-md py-0 px-[13px] font-medium">Dúvidas Frequentes</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
